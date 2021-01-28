a8b2653bc1120cea62f29d89e1117f2f9dacdbc8
Philip Davis <philipcdavis11@gmail.com>
2021-01-28 12:01:56 -0500

    Initial Commit

---
+++ PathMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PathMenu
+//
+//  Created by Philip Davis on 1/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PathMenu/PathMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathMenuApp.swift
+//  PathMenu
+//
+//  Created by Philip Davis on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct PathMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
